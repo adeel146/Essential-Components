fd96640dca887f01ab02c87b027396f0d029090a
Adeel85 <muhammad.adeel@ili.digital>
2022-10-13 10:54:33 +0500

    common textfield formik

---
+++ Netwrok Request/formikCommonTextField.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { ErrorMessage, useField, Field } from "formik";
+import PropTypes from "prop-types";
+
+const TextField = ({
+  elementType,
+  label,
+  formik,
+  width,
+  padding,
+  marginTop,
+  cardWidth,
+  showLabel,
+  textAlign,
+  onKeyDown,
+  ...props
+}) => {
+  let [field, meta] = useField(props);
+
+  return (
+    <>
+      <div
+        className="text-field"
+        style={{
+          marginTop: marginTop,
+          width: cardWidth,
+        }}
+      >
+        {showLabel === "false" ? null : (
+          <label htmlFor={field.name} className="field-label">
+            {label}
+          </label>
+        )}
+
+        <Field
+          className={`form-control shadow-none field-input ${
+            meta.touched && meta.error && "is-invalid"
+          }`}
+          autoComplete="off"
+          {...field}
+          {...props}
+          style={
+            elementType === undefined
+              ? {
+                  width: width,
+                  padding: padding,
+                  textAlign: textAlign,
+                }
+              : {
+                  width: width,
+                  padding: padding,
+                  background:
+                    "url(http://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png) no-repeat right #fff",
+                  backgroundPositionX: "95%",
+                }
+          }
+          onKeyDown={onKeyDown}
+        />
+
+        <ErrorMessage
+          component="p"
+          name={field.name}
+          style={{
+            fontSize: "11px",
+            color: "red",
+          }}
+        />
+      </div>
+    </>
+  );
+};
+TextField.propTypes = {
+  elementType: PropTypes.any,
+  label: PropTypes.any,
+  width: PropTypes.any,
+  padding: PropTypes.any,
+  marginTop: PropTypes.any,
+  cardWidth: PropTypes.any,
+  showLabel: PropTypes.any,
+};
+export default TextField;
+
+<TextField
+  type="number"
+  name="month"
+  placeholder="Monat"
+  maxLength={2}
+  max={12}
+  min={1}
+  width="100px"
+  padding="0.375rem 0.3rem"
+/>;
